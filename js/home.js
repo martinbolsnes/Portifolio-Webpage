c882f862e89efbd749179ef5e9508d3b98eed551
martinbolsnes <69841820+martinbolsnes@users.noreply.github.com>
2021-06-02 12:29:02 +0200

    added back to top btn

--- js/home.js
+++ js/home.js
@@ -1,1 +1,18 @@
+const toTopBtn = document.getElementById('toTopBtn');
 
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    toTopBtn.style.display = 'block';
+  } else {
+    toTopBtn.style.display = 'none';
+  }
+}
+
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
